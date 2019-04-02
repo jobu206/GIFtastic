a5e39b995198f41b457825236ebaee372e7d04c7
KevinL <mastahkevin@gmail.com>
2019-04-01 20:27:15 -0700

    Added more functions for rendering buttons on DOM, on page load.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@
         - This will be the "front End or HTML"
 - On load, buttons populate on DOM
     - How will buttons populate on DOM?
+        - On document load buttons will appear.
     - Hold button values in array
         - Why an array?
 - Click button and associated gifs will appear.
@@ -19,25 +20,37 @@
 // Document Ready
 $(document).ready(function () {
     console.log('ready'); 
-    
     showButtons();
 });
 
 // Global Variables
 var queryURL = 'http://api.giphy.com/v1/gifs/search?q='
-var topics = ['King of the Hill','Baseball','Scooby Doo'];
+var topics = ['King of the Hill','Beavis and Butthead','Scooby Doo','Justice League'];
 var apiKey = '21cQoZJfPW8AQIF7N8Zzhk9nf41F9ENi';
 var numOfGifs = 10;
 var rating = 'PG';
 
+      // Function for displaying movie data
+      function showButtons() {
 
-// function to create buttons
-function showButtons() {
-    // first empty the div so we don't stack buttons on each other.
-    $('#giphy-gifs').empty();
+        // Deleting the movie buttons prior to adding new movie buttons
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#giphy-btns").empty();
 
-    // loop through array of gifs
-    // for (var i = 0; i < topics.length; i++) {
-    //     const element = topics[i];    
-    // }
-}
+        // Looping through the array of movies
+        for (var i = 0; i < topics.length; i++) {
+
+          // Then dynamicaly generating buttons for each movie in the array.
+          var a = $("<button>");
+          // Adding a class
+          a.addClass("btn btn-dark");
+          // Adding a data-attribute with a value of the movie at index i
+          a.attr("data-name", topics[i]);
+          // Providing the button's text with a value of the movie at index i
+          a.text(topics[i]);
+          // Adding the button to the HTML
+          $("#giphy-btns").append(a);
+        }
+      }
+
+      $()
