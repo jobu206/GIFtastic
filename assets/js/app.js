72870a9b3e90451f5390b0bd5ed37cf90f8c8e73
KevinL <mastahkevin@gmail.com>
2019-04-01 17:03:38 -0700

    Updated JS for pseudocode and a bit of HTML for front end rendering.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,18 @@
 /*
 - Create HTML to hold buttons, search and gifs.
-- Create row and col to hold GIFs under buttons
+    - Create row and col to hold GIFs under buttons
+        - This will be the "front End or HTML"
 - On load, buttons populate on DOM
-- hold button values in array
-- Click button and associated gifs will appear via GET
+    - How will buttons populate on DOM?
+    - Hold button values in array
+        - Why an array?
+- Click button and associated gifs will appear.
+    - How will buttons appear?
+    - How will clicking a button show a GIF?
 - Clicking on gif will animate. Click again, stands still.
-
+    - GIF will first be still.
+    - First click will animate GIF
+    - Second click will make GIF stay still.
 - API Key: 21cQoZJfPW8AQIF7N8Zzhk9nf41F9ENi
 */
 
@@ -17,10 +24,10 @@ $(document).ready(function () {
 });
 
 // Global Variables
+var queryURL = 'http://api.giphy.com/v1/gifs/search?q='
 var topics = ['King of the Hill','Baseball','Scooby Doo'];
-
+var apiKey = '21cQoZJfPW8AQIF7N8Zzhk9nf41F9ENi';
 var numOfGifs = 10;
-
 var rating = 'PG';
 
 
@@ -30,4 +37,7 @@ function showButtons() {
     $('#giphy-gifs').empty();
 
     // loop through array of gifs
+    // for (var i = 0; i < topics.length; i++) {
+    //     const element = topics[i];    
+    // }
 }
